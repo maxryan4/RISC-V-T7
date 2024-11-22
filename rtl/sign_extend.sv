e6a16ec22cc4575ccfaa4e6c145c18735a75b3e4
gaoziqian123 <zg1223@ic.ac.uk>
2024-11-22 19:04:29 +0800

    control_unit changes (#5)

--- rtl/sign_extend.sv
+++ rtl/sign_extend.sv
@@ -6,8 +6,12 @@ module sign_extend (
     always_comb begin
         immop = 'x;
         case(immsrc)
-            0:immop={{21{instruction[31]}},instruction[30:20]};
-            1:immop={{20{instruction[31]}},instruction[7],instruction[30:25],instruction[11:8],1'b0};
+            0:immop={{21{instruction[31]}},instruction[30:20]};//I
+            1:immop={{20{instruction[31]}},instruction[7],instruction[30:25],instruction[11:8],1'b0};//B
+            2:immop={{21{instruction[31]}},instruction[30:25],instruction[11:7]};//S
+            3:immop={instruction[31:12],{12{1'b0}}};//U
+            4:immop={{12{instruction[31]}},instruction[19:12],instruction[20],instruction[30:25],instruction[24:21],1'b0};//j
+            
         endcase
     end
 endmodule
