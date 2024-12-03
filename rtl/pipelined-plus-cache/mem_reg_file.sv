a664f386db14271fef30f5dab073aec301957a5f
maxryan4 <maxryan428@gmail.com>
2024-12-03 20:32:08 +0000

    Added stall and flush to fetch and mem reg file

--- rtl/pipelined-plus-cache/mem_reg_file.sv
+++ rtl/pipelined-plus-cache/mem_reg_file.sv
@@ -3,6 +3,9 @@ module mem_reg_file #(
     parameter DATA_WIDTH = 32
 )(
     input logic                         clk,
+    input logic                         rst_n, // flush active low
+    input logic                         en, // stall (if set low will stall)
+
     input logic [DATA_WIDTH-1:0]        PCPlus4_m,
     input logic [DATA_WIDTH-1:0]        ALUResult_m,
     input logic [DATA_WIDTH-1:0]        ReadData_m,
@@ -16,12 +19,23 @@ module mem_reg_file #(
     output logic [1:0]                  ResultSrc_w
 );
 
-always_ff @(posedge clk) begin
-    PCPlus4_w <= PCPlus4_m;
-    ALUResult_w <= ALUResult_m;
-    ReadData_w <= ReadData_m;
-    RegWrite_w <= RegWrite_m;
-    ResultSrc_w <= ResultSrc_m;
+always_ff @(posedge clk or negedge rst_n) begin
+    if (rst_n) begin
+        if(en) begin
+            PCPlus4_w <= PCPlus4_m;
+            ALUResult_w <= ALUResult_m;
+            ReadData_w <= ReadData_m;
+            RegWrite_w <= RegWrite_m;
+            ResultSrc_w <= ResultSrc_m;
+        end
+    end
+    else begin
+        PCPlus4_w <= 32'b0;
+        ALUResult_w <= 32'b0;
+        ReadData_w <= 32'b0;
+        RegWrite_w <= 1'b0;
+        ResultSrc_w <= 2'b00;
+    end
 end
 
 endmodule
