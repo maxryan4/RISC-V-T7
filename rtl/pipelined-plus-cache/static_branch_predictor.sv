19d9de3da47c1444ef73b2e280711cd9c03330b7
alex-d-l <alexander.lewis2004@outlook.com>
2024-12-05 11:53:41 +0000

    added branch target for JAL case

--- rtl/pipelined-plus-cache/static_branch_predictor.sv
+++ rtl/pipelined-plus-cache/static_branch_predictor.sv
@@ -2,36 +2,32 @@ module static_branch_predictor #(
     DATA_WIDTH = 32
 ) (
 
-    input logic Branch_d,                    
-    input logic [DATA_WIDTH-1:0] PC_d,
-    input logic [DATA_WIDTH-1:0] ImmExt_d,      // offset for branch from decode stage
-    
-    input logic Branch_e,                       
-    input logic EQ,                             // EQ = 1 if branching
-    input logic [DATA_WIDTH-1:0] PC_e,          // PC from execute stage
-    input logic [DATA_WIDTH-1:0] ImmExt_e,      // offset for branch from execute stage
-    input logic [DATA_WIDTH-1:0] PCPlus4_e,     // PCPlus4 from execute stage
+    input logic [DATA_WIDTH-1:0] RD,
+    input logic [DATA_WIDTH-1:0] PC_f,  
+    output logic [DATA_WIDTH-1:0] branch_target,                 
+    output logic predict_taken                      // branch prediction (1 = predict branch taken)
 
-    output logic predict_taken,                 // branch prediction (1 = predict branch taken)
-    output logic [DATA_WIDTH-1:0] correct_PC    // actual PC if prediction is wrong
 );
 
-    logic [DATA_WIDTH-1:0] branch_target;       // target address of branch
+    logic [DATA_WIDTH-1:0] branch_target;           // target address of branch
 
-
-    // STATIC BRANCH PREDICTION: TAKEN FOR BACKWARD BRANCH, NOT TAKEN FOR FORWARD
     always_comb begin
-        branch_target = PC_d + ImmExt_d;             // compute branch target address
+        case (instr[6:0])
+            // Branche Instructions
+            7'b1100011: branch_target = PC_f + {{20{RD[31]}},RD[7],RD[30:25],RD[11:8],1'b0};
+            
+            // JAL
+            7'b1101111: branch_target = PC_f + {{12{RD[31]}},RD[19:12],RD[20],RD[30:21],1'b0};
 
-        if (Branch_d) begin     // if branch instruction
-            predict_taken = (branch_target < PC_d) ? 1'b1 : 1'b0;   // if target address < PC it is backward branch
-                                                                    // so assume branch IS TAKEN
+            // no case for JALR as we don't have RS1 in fetch stage yet
+
+        endcase
+        
+        if (RD[6:0] == 7'b1100011) begin            // if branch instruction
+            predict_taken = (branch_target < PC_f) ? 1'b1 : 1'b0;
         end else begin
-            predict_taken = 1'b0;               // default for non-branch instr
+            predict_taken = 1'b0;
         end
-    end 
-
-    // ACTUAL BRANCH OUTCOME
-    assign correct_PC = (Branch_e && EQ) ? (PC_e + ImmExt_e) : PCPlus4_e;   // compute correct PC in case prediction wrong
+    end
 
 endmodule
