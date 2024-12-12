b04262f627142cf922b6b74792f4fde5f5c98804
alex-d-l <alexander.lewis2004@outlook.com>
2024-12-12 14:15:28 +0000

    completed 1 bit dynamic branch predictor

--- rtl/pipelined-plus-cache/twobit_dynamic_branch_predictor.sv
+++ rtl/pipelined-plus-cache/twobit_dynamic_branch_predictor.sv
@@ -1,4 +1,4 @@
-module dynamic_branch_predictor #(
+module twobit_dynamic_branch_predictor #(
     parameter DATA_WIDTH = 32,
     parameter BTB_ROWS = 16     // number of rows in branch target buffer
 ) (
@@ -10,8 +10,8 @@ module dynamic_branch_predictor #(
     input logic                     branch_actual_taken,    // checks if branch is actually taken
     input logic [DATA_WIDTH-1:0]    branch_actual_target,
 
-    output logic                    predict_taken,
-    output logic [DATA_WIDTH-1:0]   branch_target
+    output logic                    predict_taken_f,
+    output logic [DATA_WIDTH-1:0]   branch_target_f
 
 );
 
@@ -21,7 +21,7 @@ module dynamic_branch_predictor #(
         logic [DATA_WIDTH-1:0]  tag;    // stores upper bits of PC to check entry  is current PC
         logic [DATA_WIDTH-1:0]  target; // target PC when branch taken
         logic [1:0]             pred;   // 2 bit predictor counter: 11 = ST, 10 = WT, 01 = WN, 00 = SN
-        logic                   type;    // type = 1 if *unconditional* jump
+        logic                   uncond; // uncond = 1 if *unconditional* jump
     } BTB_cols;
 
     BTB_cols BTB [BTB_ROWS-1:0];        // array of BTB entries
@@ -35,14 +35,14 @@ module dynamic_branch_predictor #(
     assign tag = PC_f[DATA_WIDTH-1:6];
 
     always_comb begin
-        predict_taken = 1'b0;           // default values
-        branch_target = PC_f;           // default values
+        predict_taken_f = 1'b0;           // default values
+        branch_target_f = PC_f;           // default values
 
-        if ((RD[6:0] == 7'b1100011) || (RD[6:0] == 7'b1101111)) begin           // check if instr is a jump
-            if (BTB[index_f].valid && (BTB[index_f].tag == tag_f)) begin        // check if entry valid and tag matches
-                if (BTB[index_f].type || (BTB[index_f].pred[1] == 1'b1)) begin  // check if uncond or ST (11) or WT (10)  
-                    predict_taken = 1'b1;                   // if unconditional inst or ST or WT then predict taken and branch  
-                    branch_target = BTB[index_f].target;
+        if ((RD[6:0] == 7'b1100011) || (RD[6:0] == 7'b1101111)) begin            // check if instr is a jump
+            if (BTB[index_f].valid && (BTB[index_f].tag == tag_f)) begin         // check if entry valid and tag matches
+                if (BTB[index_f].uncond || (BTB[index_f].pred[1] == 1'b1)) begin // check if uncond or ST (11) or WT (10)  
+                    predict_taken_f = 1'b1;                   // if unconditional inst or ST or WT then predict taken and branch  
+                    branch_target_f = BTB[index_f].target;
                 end
             end
         end
@@ -53,7 +53,7 @@ module dynamic_branch_predictor #(
         if (rst) begin
             for (int i = 0; i < BTB_ROWS; i++) begin
                 BTB[i].valid <= 1'b0;       // set valid bit of all BTB entry to 0 if rst asserted
-                BTB[i].pred  <= 2'b10       // set pred to weakly taken
+                BTB[i].pred  <= 2'b10;       // set pred to weakly taken
             end
         end
 
@@ -65,7 +65,7 @@ module dynamic_branch_predictor #(
                 BTB[update_index].valid  <= 1'b1;       
                 BTB[update_index].tag    <= branch_actual_target[DATA_WIDTH-1:6];   // update tag
                 BTB[update_index].target <= branch_actual_target;                   // update target PC
-                BTB[update_index].type   <= (RD[6:0] == 7'b1101111);                // JAL uncond. jump
+                BTB[update_index].uncond   <= (RD[6:0] == 7'b1101111);              // JAL uncond. jump
             end
 
             // this updates 2 bit predictor
